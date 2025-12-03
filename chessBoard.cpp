4b4d1f0dc7f013c3bdbffdf56af6ab1f91a5b66a
yonghoon1114 <152244262+yonghoon1114@users.noreply.github.com>
2025-12-03 14:30:01 +0000

    pawn capture logic and more movegenerator

---
+++ chessBoard.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include <cstdint>
+#define FILE_A 0x0101010101010101ULL
+#define FILE_H 0x8080808080808080ULL
+
+using namespace std;
+using U64 = uint64_t; // 64비트 unsigned 정수
+
+enum Color { WHITE, BLACK };
+
+struct Board {
+    // 12개의 기물 비트보드
+    U64 whitePawns, whiteKnights, whiteBishops, whiteRooks, whiteQueens, whiteKing;
+    U64 blackPawns, blackKnights, blackBishops, blackRooks, blackQueens, blackKing;
+
+    // 전체 occupancy
+    U64 whiteOccupancy;
+    U64 blackOccupancy;
+    U64 allOccupancy;
+
+
+    Color sideToMove;
+    uint8_t castlingRights;   // 4bit: WK, WQ, BK, BQ
+    int enPassantSquare;      // -1이면 없음
+    int halfmoveClock;
+    int fullmoveNumber;
+};
+
+void initBoard(Board &board){
+    board.whitePawns   = 0x000000000000FF00ULL; // 2번째 rank
+    board.whiteRooks   = 0x0000000000000081ULL; // a1, h1
+    board.whiteKnights = 0x0000000000000042ULL; // b1, g1
+    board.whiteBishops = 0x0000000000000024ULL; // c1, f1
+    board.whiteQueens  = 0x0000000000000008ULL; // d1
+    board.whiteKing    = 0x0000000000000010ULL; // e1
+
+    board.blackPawns   = 0x00FF000000000000ULL; // 7번째 rank
+    board.blackRooks   = 0x8100000000000000ULL; // a8, h8
+    board.blackKnights = 0x4200000000000000ULL; // b8, g8
+    board.blackBishops = 0x2400000000000000ULL; // c8, f8
+    board.blackQueens  = 0x0800000000000000ULL; // d8
+    board.blackKing    = 0x1000000000000000ULL; // e8
+
+    board.whiteOccupancy = board.whitePawns | board.whiteKnights | board.whiteBishops
+                         | board.whiteRooks | board.whiteQueens | board.whiteKing;
+
+    board.blackOccupancy = board.blackPawns | board.blackKnights | board.blackBishops
+                         | board.blackRooks | board.blackQueens | board.blackKing;
+
+    board.allOccupancy = board.whiteOccupancy | board.blackOccupancy;
+
+    board.sideToMove = WHITE;
+    board.castlingRights = 0b1111; // 모두 가능
+    board.enPassantSquare = -1;
+    board.halfmoveClock = 0;
+    board.fullmoveNumber = 1;
+};
+
+U64 whitePawnMove(U64 whitePawn, U64 allOccupied){
+    U64 empty = ~allOccupied;
+    U64 singlePush = (whitePawn<<8)&empty;
+    U64 doublePush = ((singlePush & 0x0000000000FF0000ULL) << 8) & empty;
+    
+    return singlePush | doublePush;
+};
+
+U64 blackPawnMove(U64 blackPawn, U64 allOccupied){
+    U64 empty = ~allOccupied;
+    U64 singlePush = (blackPawn>>8)&empty;
+    U64 doublePush = ((singlePush & 0x0000FF0000000000ULL) >> 8) & empty;
+    
+    
+    return singlePush | doublePush;
+};
+
+U64 kingMove(U64 blackKing, U64 blackOccupancy){
+
+    U64 canMove = ~blackOccupancy;
+    U64 goLeft = (blackKing<<1) & canMove & FILE_A ;
+    U64 goRight = (blackKing>>1) & canMove & FILE_H ;
+    U64 goUp = (blackKing<<8) & canMove;
+    U64 goDown = (blackKing>>8) & canMove;
+
+    return goLeft|goRight|goUp|goDown;
+
+};
